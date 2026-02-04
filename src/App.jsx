769fd963868319e96f71f4ccc73929f00e7687c4
Bmwm3 <jhon.bmwm3competition@gmail.com>
2026-02-04 11:46:27 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+ export default  function App() {
+
+  return (
+    <>
+        <section>
+        <textarea name="placeholder" id="descreva seu problema">
+          <button>Enviar</button>
+        </textarea>
+        </section>
+      </> 
+  )
+ }
